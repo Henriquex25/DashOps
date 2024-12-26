604dd24a8083b83a84f7d49005f5e68e31643ffa
Henrique <sergioh003@gmail.com>
2024-12-26 03:22:19 -0300

    first commit

---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium aliquam aliquid culpa debitis delectus
+        dolore nesciunt, nihil nisi numquam, obcaecati officia provident quas, quidem sit. Deleniti molestias quod
+        vero!</p>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import preset from './vendor/filament/support/tailwind.config.preset'
+import forms from '@tailwindcss/forms';
+import colors from "tailwindcss/colors.js";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    presets: [preset],
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        './app/Filament/**/*.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: colors.sky,
+            }
+        },
+    },
+
+    plugins: [forms],
+};
